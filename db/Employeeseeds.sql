3f8b8b556d700f11a51304ed29e98f141215bfee
Ninamahdawe <ninamahdawe11@gmail.com>
2023-09-07 15:52:57 -0700

    seeding the data to the empolyeeseeding.sql file

--- db/Employeeseeds.sql
+++ db/Employeeseeds.sql
@@ -1,2 +1,15 @@
 INSERT INTO departments (name)
-VALUES ("Data Science"), ("Legal"), ("Client sucess"), ("Engineering"), ("Human Resources");
+VALUES ("Accounting"), ("Legal"), ("Management"), ("Engineering"), ("Human Resources");
+
+INSERT INTO
+    roles (title, salary, department_id)
+VALUES ("Solutions Engineer", 11000, 3), ("Software Engineer", 11.662, 3), ("Staff Accountant", 6500, 2), ("Sales Engineer", 8000, 3);
+
+INSERT INTO
+    employees (
+        first_name,
+        last_name,
+        role_id,
+        manager_id
+    )
+VALUES ()
