6b13ef5c426d4f858922834ef81cdf4e00a7bac1
Ninamahdawe <ninamahdawe11@gmail.com>
2023-09-07 15:27:49 -0700

    added the EMPLOYEES table to the schema.sql file

--- db/schema.sql
+++ db/schema.sql
@@ -19,3 +19,17 @@ CREATE TABLE
         FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE
         SET NULL
     );
+
+CREATE TABLE
+    EMPLOYEES (
+        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        first_name VARCHAR(30) NOT NULL,
+        last_name VARCHAR(30) NOT NULL,
+        role_id INT,
+        manager_id INT NULL,
+        FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE
+        SET
+            NULL,
+            FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE
+        SET NULL
+    );
