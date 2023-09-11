ab2a3e4db65a96e22f53522be17723b16d50ba02
Ninamahdawe <ninamahdawe11@gmail.com>
2023-09-11 15:29:49 -0700

    updating the connection.js file with the createconnection function

--- db/connection.js
+++ db/connection.js
@@ -2,14 +2,13 @@ const mysql = require('mysql2');
 
 
 // Connect to database
-const db = mysql.createConnection(
-    {
-        host: 'localhost',
-        user: 'root',
-        password: 'Mahdawe11@@',
-        database: 'teams_db'
-    },
-    console.log(`Connected to the teams_db database.`)
-);
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'your-username',
+    password: 'your-password',
+    database: 'teams_db',
+});
+console.log(`Connected to the teams_db database.`)
+
 
 module.exports = db
