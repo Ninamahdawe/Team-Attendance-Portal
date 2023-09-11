b3ed800a5b375ed4d9705fbe4b638bca676cebfa
Ninamahdawe <ninamahdawe11@gmail.com>
2023-09-11 14:42:52 -0700

    adding the catch err to the function

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const figletAsync = util.promisify(figlet);
 async function generateHeader() {
     try {
         const header = await figletAsync("Team Portal");
+        return header;
+    } catch (err) {
         // First when our app starts we will display a welcome message.
         // Next we will ask them to choose an operation
         // with their choice we will call handUserChoice and pass the choice
