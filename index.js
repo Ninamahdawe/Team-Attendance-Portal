0472a69cc96505d3a7aa620beba57841fa664ea7
Ninamahdawe <ninamahdawe11@gmail.com>
2023-09-11 14:41:22 -0700

    installed the util dependinces

--- index.js
+++ index.js
@@ -2,17 +2,18 @@ const inqurer = require("inquirer");
 const userQuestions = require("./questions/questions")
 const figlet = require("figlet");
 const figletAsync = util.promisify(figlet);
+async function generateHeader() {
+    try {
+        // First when our app starts we will display a welcome message.
+        // Next we will ask them to choose an operation
+        // with their choice we will call handUserChoice and pass the choice
+        // the handUserChoice function will route our application to the correct function
+        // then the opperation will be executed ( may reqire more prompting )
+        // after they results of the operation are completed you will return them to the menu that ask for their selected operation
 
-// First when our app starts we will display a welcome message.
-// Next we will ask them to choose an operation
-// with their choice we will call handUserChoice and pass the choice
-// the handUserChoice function will route our application to the correct function
-// then the opperation will be executed ( may reqire more prompting )
-// after they results of the operation are completed you will return them to the menu that ask for their selected operation
+        function init() {
+            console.log("Welcome To the Team Portal!");
+            userQuestions()
 
-function init() {
-    console.log("Welcome To the Team Portal!");
-    userQuestions()
-
-}
-init()
+        }
+        init()
