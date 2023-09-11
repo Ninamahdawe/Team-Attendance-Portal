463873ebbe0bc0009aeca82596374c024e934092
Ninamahdawe <ninamahdawe11@gmail.com>
2023-09-11 14:43:19 -0700

    console logging the error

--- index.js
+++ index.js
@@ -7,16 +7,20 @@ async function generateHeader() {
         const header = await figletAsync("Team Portal");
         return header;
     } catch (err) {
-        // First when our app starts we will display a welcome message.
-        // Next we will ask them to choose an operation
-        // with their choice we will call handUserChoice and pass the choice
-        // the handUserChoice function will route our application to the correct function
-        // then the opperation will be executed ( may reqire more prompting )
-        // after they results of the operation are completed you will return them to the menu that ask for their selected operation
+        console.log("Error:", err); // Log any errors
+        return null;
+    }
+}
+// First when our app starts we will display a welcome message.
+// Next we will ask them to choose an operation
+// with their choice we will call handUserChoice and pass the choice
+// the handUserChoice function will route our application to the correct function
+// then the opperation will be executed ( may reqire more prompting )
+// after they results of the operation are completed you will return them to the menu that ask for their selected operation
 
-        function init() {
-            console.log("Welcome To the Team Portal!");
-            userQuestions()
+function init() {
+    console.log("Welcome To the Team Portal!");
+    userQuestions()
 
-        }
-        init()
+}
+init()
